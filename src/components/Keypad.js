210dc74fb956e64a8c58728c31584091d7dd83f4
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-06-13 10:56:43 -0400

    commit tree f8172ec91c084ad700630549c86c3f026ff00e3f

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react'
+//import ReactDOM from 'react-dom';
+
+/*const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  ); */
+
+function Keypad() {
+    const password = (
+        <input type="password" onChange ={console.log('Entering password...')}/>
+    )
+    //root.render(password);
+    return password
+}
+
+export default Keypad;
